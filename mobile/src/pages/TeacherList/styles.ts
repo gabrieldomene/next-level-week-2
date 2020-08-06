c608020041aae7a4037fbd4d5b471deab6fd6161
Gabriel Domene <domenee.g@gmail.com>
2020-08-06 12:23:57 -0300

    mobile app layout

---
+++ mobile/src/pages/TeacherList/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { setStatusBarBackgroundColor } from "expo-status-bar";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f7',
+  }
+})
+
+export default styles;
